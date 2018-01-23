1f942340303eaefb89da4fbfe3b8fc0ef5f8c7c3
Mocoma <mocomaminecraft@gmail.com>
2018-01-23 15:39:58 +0100

    bot now sends tweets

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # postubot
 Currently a test, but may be something bigger later
+
+Sends a tweet every hour, the text is whatever you introduce via input()
--- bot.py
+++ bot.py
@@ -15,4 +15,22 @@
 #   You should have received a copy of the GNU General Public License
 #   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import tweepy, 
+from secrets import *
+import time
+import tweepy
+
+#variables
+TIMEBETWEENTWEETS = 60 * 60
+
+def getTwitter():
+    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+    auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
+
+    return tweepy.API(auth)
+
+#for a test, send a tweet every hour
+while True:
+    getTwitter().update_status(status = input("enter tweet to send: "))
+    print("sent")
+
+    time.sleep(TIMEBETWEENTWEETS)
